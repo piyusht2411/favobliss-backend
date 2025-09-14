91ea9f35facee4070a8b48c8fcd9f17d64fcf917
Piyush Thakur <piyushthakur241199@gmail.com>
2025-09-15 00:03:49 +0530

    changes

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,15 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalThis.prisma ||
+  new PrismaClient({
+    log: ["error", "query"], // Add 'query' to debug DB calls
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // Directly use Amplify's env var
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
